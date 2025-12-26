08221a15522833b7d333d1e3c8a38440b8a736bd
‫Sahar Admoni <64500341+saharad1@users.noreply.github.com>
2025-12-26 17:13:03 +0200

    updates

--- projects/sysllm/static/js/index.js
+++ projects/sysllm/static/js/index.js
@@ -67,13 +67,13 @@ $(document).ready(function () {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-  preloadInterpolationImages();
-
-  $("#interpolation-slider").on("input", function (event) {
-    setInterpolationImage(this.value);
-  });
-  setInterpolationImage(0);
-  $("#interpolation-slider").prop("max", NUM_INTERP_FRAMES - 1);
+  // Interpolation code commented out - section removed from HTML
+  // preloadInterpolationImages();
+  // $("#interpolation-slider").on("input", function (event) {
+  //   setInterpolationImage(this.value);
+  // });
+  // setInterpolationImage(0);
+  // $("#interpolation-slider").prop("max", NUM_INTERP_FRAMES - 1);
 
   bulmaSlider.attach();
 });
